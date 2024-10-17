2a7539486990629c8f88f4c2e15bd271c5f690bf
Annmon James <annmonjameschiramel@gmail.com>
2024-10-17 10:12:03 +0530

    gatts_server code base

---
+++ gatt_server/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/annmo/esp/v5.3/esp-idf/components/bootloader/subproject"
+  "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader"
+  "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix"
+  "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix/tmp"
+  "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix/src"
+  "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Lectrix_company/work/Git_projects/Embedded_test/gatt_server/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
